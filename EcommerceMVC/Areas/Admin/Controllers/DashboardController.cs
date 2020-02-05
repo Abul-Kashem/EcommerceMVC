2803523dc6b3f1af5565b137858c479689907ae8
Abul Kashem <kashem.mcpd@gmail.com>
2020-02-05 15:11:31 +0600

    Part 2 Create admin Layout

---
+++ EcommerceMVC/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceMVC.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/dashboard")]
+    public class DashboardController : Controller
+    {
+        [Route("")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
